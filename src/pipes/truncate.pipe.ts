25a5640d9b1bf3ee56f3346d1aebaa736f3bc396
Bojan <bojan.kogoj@gmail.com>
2016-11-03 14:01:47 +0100

    added left truncate

--- src/pipes/truncate.pipe.ts
+++ src/pipes/truncate.pipe.ts
@@ -6,10 +6,15 @@ import { Injectable, PipeTransform, Pipe } from '@angular/core';
 })
 @Injectable()
 export class TruncatePipe implements PipeTransform {
-  transform(value: string, limit: number, trail: string) : string {
+  transform(value: string, limit: number, trail: string, position: string): string {
     limit = limit || 10;
     trail = trail || '...';
+    position = position || 'right';
 
-    return value.length > limit ? value.substring(0, limit) + trail : value;
+    if (position === 'left') {
+      return value.length > limit ? trail + value.substring(value.length - limit, value.length) : value;
+    } else {
+      return value.length > limit ? value.substring(0, limit) + trail : value;
+    }
   }
 }
