be9bf247ec5e4f36ed9614a9920e38e84d7c9b35
Sylvain RAGOT <sylvain.ragot@predicsis.com>
2016-11-07 16:36:22 +0100

    Add the 'middle' value for the `position` parameter

--- src/pipes/truncate.pipe.ts
+++ src/pipes/truncate.pipe.ts
@@ -12,9 +12,19 @@ export class TruncatePipe implements PipeTransform {
     position = position || 'right';
 
     if (position === 'left') {
-      return value.length > limit ? trail + value.substring(value.length - limit, value.length) : value;
+      return value.length > limit
+        ? trail + value.substring(value.length - limit, value.length)
+        : value;
+    } else if (position === 'right') {
+      return value.length > limit
+        ? value.substring(0, limit) + trail
+        : value;
+    } else if (position === 'middle') {
+      return value.length > limit
+        ? value.substring(0, limit/2) + trail + value.substring(value.length - limit/2, value.length)
+        : value;
     } else {
-      return value.length > limit ? value.substring(0, limit) + trail : value;
+      return value;
     }
   }
 }
